9e4b3db2198ff14f052578d9fb8e311369d971ee
廖幸杰 <584566770@qq.com>
2016-07-28 15:12:29 +0800

    sdf

---
+++ XJ/CTFramework.framework/Headers/CTFramework.h
@@ -0,0 +1,20 @@
+//
+//  CTFramework.h
+//  CTFramework
+//
+//  Created by gn_macMini_liao on 16/7/28.
+//  Copyright © 2016年 gn_macMini_liao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+////! Project version number for CTFramework.
+//FOUNDATION_EXPORT double CTFrameworkVersionNumber;
+//
+////! Project version string for CTFramework.
+//FOUNDATION_EXPORT const unsigned char CTFrameworkVersionString[];
+//
+//// In this header, you should import all the public headers of your framework using statements like #import <CTFramework/PublicHeader.h>
+
+#import <CTFramework/CTModel.h>
+
